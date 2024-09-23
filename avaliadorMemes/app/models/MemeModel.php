f0ef78d6e81c141feb572495d404c65ba549e4ea
Zgplay056 <gabriel.scharnoski@alunos.sc.senac.br>
2024-09-23 20:14:47 -0300

    Fiz um CRUD no memeModel

--- avaliadorMemes/app/models/MemeModel.php
+++ avaliadorMemes/app/models/MemeModel.php
@@ -1,5 +1,5 @@
 <?php
-    Class MemeModel //nome no controller ta errado
+    Class MemeModel //Essa Classe mexe com a tabela Memes
     {
         private $db;
 
@@ -13,5 +13,39 @@
             echo"função index";
         }
 
-        
+        public function getAllMemes() {
+           $query = $this->$db->query('SELECT * FROM memes ORDER BY media_avaliacao DESC');
+
+           return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function getById($id) {
+            $query = $this->db->prepare("SELECT * FROM memes WHERE id = :id");
+    
+            $query->execute(['id' => $id]);
+    
+           
+            return $query->fetch(PDO::FETCH_ASSOC);
+        }
+
+        public function insert($data) {
+            $query = $this->db->prepare("INSERT INTO memes (titulo, imagem_url, imagem_update, media_avaliacao) VALUES (:titulo, :imagem_url, :imagem_update, :media_avaliacao)");
+    
+            return $query->execute($data);
+        }
+
+        public function update($data) {
+            $query = $this->db->prepare("UPDATE INTO memes (titulo, imagem_url, imagem_update, media_avaliacao) VALUES (:titulo, :imagem_url, :imagem_update, :media_avaliacao)");
+    
+            return $query->execute($data);
+        }
+
+        public function delete($id)  {  
+        $query = $this->db->prepare("DELETE FROM memes WHERE id = :id");
+
+        return $query->execute(['id' => $id]);
     }
+    }
+    
+
+        
