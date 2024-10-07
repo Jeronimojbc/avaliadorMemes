c3018e3a59bfabf738eae9f69edd36803776973d
Zgplay056 <gabrielgomesplay056@gmail.com>
2024-10-07 19:46:58 -0300

    Fiz a rota para o show funcionar apos o cadastro

--- avaliadorMemes/index.php
+++ avaliadorMemes/index.php
@@ -1,19 +1,21 @@
 <?php
+//chama o database pra conectar
     require_once 'app/core/Database.php';
 
+#Chama o MemeController para ele funcionar   
     require_once 'app/controllers/MemesController.php';
 
 
 
-    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); #pega a URI e coloca em uma var
 
-    if ($uri === '/' || $uri === '/index.php'){
-        $controller = new MemesController();
-         $controller->index();
+    if ($uri === '/' || $uri === '/index.php'){ #checa se depois da barra tem algo
+        $controller = new MemesController();# cria um obj do controller pra usar as funções
+         $controller->index(); #chama a função index
 
-    } elseif ($uri === '/memes'){
+    } elseif ($uri === '/memes'){ 
         $controller = new MemesController();
-        $controller->create();
+        $controller->create(); # se for a URI memes chama a função create()
 
     }elseif ($uri === '/memes/store'){
         $controller = new MemesController();
@@ -29,10 +31,10 @@
         print_r($matches);
         $controller->show($matches[1]);
     }    
-
-
     /* Rota para avaliar*/     
-    }elseif ($uri === 'rate.php')
+    elseif ($uri === 'rate.php') {
+        
+    }
 
 
 
