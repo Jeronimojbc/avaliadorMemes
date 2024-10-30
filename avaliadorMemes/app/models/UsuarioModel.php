35809b5a7329dd2d9cfe0b31dcbbb140ea6fe711
Zgplay056 <gabrielgomesplay056@gmail.com>
2024-10-29 21:38:52 -0300

    Feita a correção do meu ultimo commit, falta finalização do login

--- avaliadorMemes/app/models/UsuarioModel.php
+++ avaliadorMemes/app/models/UsuarioModel.php
@@ -1,17 +1,23 @@
 <?php
+
 class UsuarioModel {
+
     private $db;
 
     public function __construct($db) {
         $this->db = Database::getConnection();
+        echo"UserModel Funcionando";
     }
 
-    public function loginModel($email, $senha) {
-        $query = "SELECT * FROM users WHERE email = :email AND senha = :senha";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':senha', $senha);
-        $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+    public function getAll() {
+        $query = $this->db->prepare("SELECT * FROM usuarios WHERE email = :email AND senha = :senha"); # prepara o comando
+        $query->execute(); #faaz o SELECT nesse caso
+        return $query->fetchAll(PDO::FETCH_OBJ); # retorna
+    }
+    public function getById($id) {
+        $query = $this->db->prepare("SELECT * FROM usuarios WHERE id = :id");
+        $query->bindParam(':id', $id);
+        $query->execute();
+        return $query->fetch(PDO::FETCH_OBJ);
     }
 }
--- avaliadorMemes/app/views/usuarios/login.php
+++ avaliadorMemes/app/views/usuarios/login.php
@@ -1,22 +1,31 @@
-<?php
-class Views {
-    public function renderLogin() {
-        echo '
-        <form method="POST" action="">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php if (isset($erroooou)): // se a var de erro existir ele nem loga?> 
+            <div role="alert">
+                <?= $erroooou ?>
+            </div>
+        <?php endif; ?>
+
+
+        <form method="POST" action="/login">
             <label for="username">Nome de usuário:</label>
             <input type="text" id="username" name="username">
             <label for="password">Senha:</label>
             <input type="password" id="password" name="password">
             <button type="submit">Entrar</button>
+
+            <?php foreach ($usuarios as $user): ?>
+                 <option value="<?= $user->id ?>"><?=($user->nome) ?></option>
+     <?php endforeach; ?>
+
         </form>
-        ';
-    }
-
-    public function renderSuccess() {
-        echo "Login bem-sucedido!";
-    }
-
-    public function renderError() {
-        echo "Nome de usuário ou senha incorretos.";
-    }
-}
+</body>
+</html>
